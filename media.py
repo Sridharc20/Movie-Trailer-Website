d800e94b54e6d41c99d271e14e29bfa85ed6867f
Sridhar C <sridharc20@gmail.com>
2017-07-17 21:59:33 +0530

    Update media.py

--- media.py
+++ media.py
@@ -1,26 +1,27 @@
 """
 Module to display movie object, attributes and instances
 """
-
 import webbrowser
 
 
 class Movie():
-	"""This is a movie class which takes the input parameters (movie_title,movie_storyline,poster_image,trailer_youtube,movie_rating) """
-
-
-	VALID_RATINGS = ["G","PG","PG-13","R"]
-	#constructor
-	def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube,movie_rating):
-		#instance variables
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-		self.rating = movie_rating
-	#Instance method
-	def show_trailer(self):
-		""" This method will show the trailer of the selected movie"""
-		webbrowser.open(self.trailer_youtube_url)
-
-
+    """
+    This is a movie class which takes the input parameters
+    (movie_title,movie_storyline,poster_image,trailer_youtube,movie_rating)
+    """
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+    
+    def __init__(self, title, storyline, image_url, youtube_url, rating):
+        """
+        This is the initialization method
+        """
+        self.title = title
+        self.storyline = storyline
+        self.image_url = image_url
+        self.youtube_url = youtube_url
+        self.rating = movie_rating
+  
+    def show_trailer(self):
+        """
+        This method will show the trailer of the selected movie on the browser
+        """
